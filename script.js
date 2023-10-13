00db62fb61b52879c4c36a27d53287fbbd2821b9
Alex Rofes <alexrofes1992@gmail.com>
2023-10-13 14:58:05 +0200

    pause

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ audioControl.addEventListener('click', () => {
         audioControl.innerHTML = '<i class="fas fa-volume-up"></i>';
     }
 });
+
+let juegoPausado = false;
+
+function togglePausa() {
+    console.log("Toggle pausa"); // Agregar un mensaje de consola para verificar
+    juegoPausado = !juegoPausado;
+
+    if (juegoPausado) {
+        noLoop(); // Pausar el juego
+        document.getElementById('pausaControl').style.display = 'none';
+        document.getElementById('playControl').style.display = 'block'; // Muestra el botón "Play"
+    } else {
+        loop(); // Reanudar el juego
+        document.getElementById('pausaControl').style.display = 'block';
+        document.getElementById('playControl').style.display = 'none'; // Oculta el botón "Play"
+    }
+}
+
+
+document.getElementById('pausaControl').addEventListener('click', togglePausa);
+document.getElementById('playControl').style.display = 'block'; // Muestra el botón "Play"
+document.getElementById('playControl').addEventListener('click', togglePausa);
+document.getElementById('playControl').style.display = 'none'; // Oculta el botón "Play"
+
+
+
+
